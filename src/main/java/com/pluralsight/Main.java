36b943b97c64164a936a15fbbd9baf603a88fbc8
jihaddrake <ringod40@gmail.com>
2026-04-23 14:47:42 -0400

    Did a hashmap

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,27 @@
 package com.pluralsight;
+import java.util.HashMap;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+    public static void loadInventory() {
+
+        HashMap<Double, String> inventory = new HashMap<Double, String>();
+
+        inventory.put(20.99, "Gauntlet");
+        inventory.put(11.00, "Chesse");
+        inventory.put(9.99, "lemon");
+        inventory.put(30.00, "Samson");
+        inventory.put(31.01, "Fish");
+
+
+
+
+
+    }
 }
